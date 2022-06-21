de617d1a827e15547f04f196c1d17326d0b17c15
Daniel Machado Barra <daniel.barra@bancointer.com.br>
2022-06-21 16:36:08 -0300

    kills and killers to a dictonary
    -Add killers and kills to the dictionary

--- extractors/game_data_extractor.py
+++ extractors/game_data_extractor.py
@@ -1,3 +1,6 @@
+from dictionary.merge import merge
+from extractors.killer_data_extractor import extract_deaths
+
 
 def is_game_starter(text):
     return 'InitGame' in text
@@ -7,6 +10,10 @@ def is_game_end(text):
     return 'ShutdownGame' in text
 
 
+def is_game_kill(text):
+    return 'Kill' in text
+
+
 def split_game_data(overall_dict, game_dict, lines):
     for line in lines:
         if is_game_starter(line):
@@ -14,3 +21,5 @@ def split_game_data(overall_dict, game_dict, lines):
             game_dict['total_kills'] = 0
         if is_game_end(line):
             overall_dict[len(overall_dict)+1] = game_dict.copy()
+        if is_game_kill(line):
+            merge(game_dict, extract_deaths(line))
--- extractors/test_game_data_extractor.py
+++ extractors/test_game_data_extractor.py
@@ -1,4 +1,4 @@
-from extractors.game_data_extractor import is_game_starter, is_game_end, split_game_data
+from extractors.game_data_extractor import is_game_starter, is_game_end, split_game_data, is_game_kill
 
 
 def test_should_confirm_that_is_a_starter():
@@ -35,3 +35,61 @@ def test_should_add_game_into_dictionary():
     split_game_data(overall_dict, game_dict, lines)
     assert len(overall_dict) == 1
     assert game_dict['total_kills'] == 0
+
+
+def test_should_confirm_that_is_a_kill():
+    text = '23:06 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT'
+    assert is_game_kill(text)
+
+
+def test_should_not_confirm_that_is_a_kill():
+    text = 'lientUserinfoChanged: 2 nIsgalamidomodel'
+    assert not is_game_kill(text)
+
+
+def test_should_add_game_and_kills():
+    overall_dict = {}
+    game_dict = {}
+    lines = """  0:00 InitGame: xasdasdasdasd \n
+                15:00 Exit: Timelimit hit. \n
+                20:34 ClientConnect: 2 \n
+                20:54 Kill: 1022 2 22: <world> killed leo by MOD_TRIGGER_HURT \n
+                20:34 ClientUserinfoChanged: 2 asdasdasdasd \n
+                20:37 ClientUserinfoChanged: 2 asdasdasdad \n
+                20:37 ClientBegin: 2 \n
+                20:37 ShutdownGame: \n
+            """.split('\n')
+    split_game_data(overall_dict, game_dict, lines)
+    assert len(overall_dict) == 1
+    assert overall_dict[1]['total_kills'] == 1
+    assert overall_dict[1]['leo'] == -1
+
+
+def test_should_add_game_and_kills_for_multi_games():
+    overall_dict = {}
+    game_dict = {}
+    lines = """  0:00 InitGame: xasdasdasdasd \n
+                15:00 Exit: Timelimit hit. \n
+                20:34 ClientConnect: 2 \n
+                20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT \n
+                20:34 ClientUserinfoChanged: 2 asdasdasdasd \n
+                20:37 ClientUserinfoChanged: 2 asdasdasdad \n
+                20:37 ClientBegin: 2 \n
+                20:37 ShutdownGame: \n
+                
+                0:00 InitGame: xasdasdasdasd \n
+                15:00 Exit: Timelimit hit. \n
+                20:34 ClientConnect: 2 \n
+                22:06 Kill: 2 3 7: Isgalamido killed Mocinha by MOD_ROCKET_SPLASH \n
+                20:34 ClientUserinfoChanged: 2 asdasdasdasd \n
+                20:37 ClientUserinfoChanged: 2 asdasdasdad \n
+                20:37 ClientBegin: 2 \n
+                20:37 ShutdownGame: \n
+            """.split('\n')
+    split_game_data(overall_dict, game_dict, lines)
+    assert len(overall_dict) == 2
+    assert overall_dict[1]['total_kills'] == 1
+    assert overall_dict[1]['Isgalamido'] == -1
+    assert len(overall_dict) == 2
+    assert overall_dict[2]['total_kills'] == 1
+    assert overall_dict[2]['Isgalamido'] == 1
